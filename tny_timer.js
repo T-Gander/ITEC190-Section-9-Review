69238dcb3d58b4dd49021fc1389c43a52820967c
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-03-26 13:43:42 -0600

    Finished

--- tny_timer.js
+++ tny_timer.js
@@ -10,22 +10,32 @@
    Date:   26/03/2023
 
 */
-var j4Date = nextJuly4(thisDay);
-j4Date.setHours(21);
-
-var days;
-var hrs;
-var mins;
-var secs;
-
 var thisDay = new Date("May 19, 2021 09:31:27");
+var j4Date = nextJuly4(thisDay).setHours(21);
 
-function showClock(){
-   var localDate = thisDay.toLocaleDateString();
-   var localTime = thisDay.toLocaleTimeString();
+showClock(thisDay);
+setInterval("runClock()", 1000);
 
-   document.getElementById("currentTime").textContent =
-   localDate + " " + localTime;
+function showClock(thisDay){
+   
+   var dateStr = thisDay.toLocaleDateString();
+   var timeStr = thisDay.toLocaleTimeString();
+
+   document.getElementById("date").textContent = dateStr;
+   document.getElementById("time").textContent = timeStr;
+
+   /* Calculate the days until July 4th, at 9PM */
+   var daysLeft = (j4Date - thisDay)/(1000*60*60*24);
+   /* Calculate the hours left in the current day */
+   var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+   /* Calculate the minutes and seconds left in the current hour */
+   var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+   var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+
+   document.getElementById("dLeft").textContent = Math.floor(daysLeft);
+   document.getElementById("hLeft").textContent = Math.floor(hrsLeft);
+   document.getElementById("mLeft").textContent = Math.floor(minsLeft);
+   document.getElementById("sLeft").textContent = Math.floor(secsLeft);
 }
 
 function nextJuly4(currentDate) {
