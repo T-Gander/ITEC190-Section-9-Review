e3691cd51997224aa27110cc3bf6b98a969fc36a
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-03-26 12:42:01 -0600

    Up to section 5f

--- tny_timer.js
+++ tny_timer.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /*
    New Perspectives on HTML5 and CSS3, 8th Edition
    Tutorial 9
@@ -8,7 +10,23 @@
    Date:   26/03/2023
 
 */
+var j4Date = nextJuly4(thisDay);
+j4Date.setHours(21);
 
+var days;
+var hrs;
+var mins;
+var secs;
+
+var thisDay = new Date("May 19, 2021 09:31:27");
+
+function showClock(){
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
+
+   document.getElementById("currentTime").textContent =
+   localDate + " " + localTime;
+}
 
 function nextJuly4(currentDate) {
    var cYear = currentDate.getFullYear();
